70846923cfe1e1cf8231004abb87f88de01cf4fd
Michael Mamuric <michael.mamuric@edu.sait.ca>
2021-01-17 10:24:09 -0700

    Data fertching on Server Side

--- pages/[country]/index.js
+++ pages/[country]/index.js
@@ -1,14 +1,19 @@
-import { useEffect } from 'react';
 import axios from 'axios';
 
-const Country = () => {
+// Server Side Rendering
+export const getServerSideProps = async() => {
+    const response = await axios.get('http://api.tvmaze.com/schedule?country=JP');
+    
+    // Response obtained from API call will be sent to Country as props
+    return {
+        props: {
+            shows: response.data
+        }
+    }
+}
 
-    // Traditional componentDidMount
-    // Will fetch data on client side
-    useEffect(async() => {
-        const response = await axios.get('http://api.tvmaze.com/schedule?country=JP')
-        console.log('Response', response)
-    }, [])
+const Country = (props) => {
+    console.log(props.shows);
 
     return (
         <h1>Country Test</h1>
