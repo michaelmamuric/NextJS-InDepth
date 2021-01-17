cbb66f09a4891d411781538389c9bb0b5a10df0d
Michael Mamuric <michael.mamuric@edu.sait.ca>
2021-01-17 09:13:31 -0700

    Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      This is my homepage.
+    </div>
+  )
+}
+
+export default Home
