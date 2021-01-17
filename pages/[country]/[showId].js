5719144e148431ee7038647c6c9107d077e0ad5f
Michael Mamuric <michael.mamuric@edu.sait.ca>
2021-01-17 09:23:06 -0700

    Dynamic Routing For Country and Show Id

---
+++ pages/[country]/[showId].js
@@ -0,0 +1,7 @@
+const ShowDetails = () => {
+    return (
+        <h1>Show Details</h1>
+    )
+}
+
+export default ShowDetails;
---
+++ pages/[country]/index.js
@@ -0,0 +1,7 @@
+const Country = () => {
+    return (
+        <h1>Country Test</h1>
+    )
+}
+
+export default Country;
