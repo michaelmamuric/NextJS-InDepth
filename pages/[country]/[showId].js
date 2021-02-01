d22b9ea7ba66129e2fd6d370ca8abe323c17a1d2
Michael Mamuric <michael.mamuric@edu.sait.ca>
2021-01-31 17:13:08 -0700

    Show Details - Initial Implementation

--- pages/[country]/[showId].js
+++ pages/[country]/[showId].js
@@ -1,6 +1,34 @@
-const ShowDetails = () => {
+import classes from './index.module.css';
+import axios from 'axios';
+
+export const getServerSideProps = async(context) => {
+    try {
+        const response = await axios.get('http://api.tvmaze.com/shows/1?embed=cast');
+        return {
+            props: {
+                show: response.data
+            }
+        }
+    } catch(error) {
+        return {
+            props: {
+                error: error.error
+            }
+        }
+    }
+}
+
+const ShowDetails = (props) => {
+    // Destructuring
+    const { name, image } = props.show;
+
     return (
-        <h1>Show Details</h1>
+        <div className={classes.ShowDetail}>
+            <div className={classes.Poster}
+                style={{ backgroundImage: `url(${image.original})` }}
+            ></div>
+            <h1>{name}</h1>
+        </div>
     )
 }
 
